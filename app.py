027190bc27c78b9444c34402a99a9169b4dfcd40
deonbola12-tech <deonbola12@gmail.com>
2025-09-09 16:20:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for homepage
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Makes the server visible externally
+    app.run(host="0.0.0.0", port=5000, debug=True)
